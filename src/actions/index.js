a893e7c9264abc898cb197c30b948c182d98dc90
RyanDBarnett <conner2468@gmail.com>
2019-07-13 13:30:09 -0600

    Create setError action

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,9 +1,14 @@
 export const setAllResources = (resources) => ({
   type: 'SET_ALL_RESOURCES',
   resources
-})
+});
 
 export const setLoading = (bool) => ({
   type: 'SET_LOADING',
   isLoading: bool
-})
+});
+
+export const setError = (error) => ({
+  type: 'SET_ERROR',
+  error
+});
