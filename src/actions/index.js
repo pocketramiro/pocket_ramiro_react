60b4038389147b8a7faae17790b07c48d3a3e261
RyanDBarnett <conner2468@gmail.com>
2019-07-13 13:29:16 -0600

    Create setLoading action

--- src/actions/index.js
+++ src/actions/index.js
@@ -2,3 +2,8 @@ export const setAllResources = (resources) => ({
   type: 'SET_ALL_RESOURCES',
   resources
 })
+
+export const setLoading = (bool) => ({
+  type: 'SET_LOADING',
+  isLoading: bool
+})
