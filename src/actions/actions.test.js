f63179e70f85b6f60b25547c3df8adda46f2e182
RyanDBarnett <conner2468@gmail.com>
2019-07-13 13:33:44 -0600

    Create setLoading test

--- src/actions/actions.test.js
+++ src/actions/actions.test.js
@@ -1,6 +1,7 @@
 import * as actions from './';
 
 describe('actions', () => {
+  
   describe('setAllResources', () => {
     it('should return a type of setAll with a projects array', () => {
       const resources = [
@@ -13,6 +14,20 @@ describe('actions', () => {
       const expected = {type: 'SET_ALL_RESOURCES', resources};
 
       const result = actions.setAllResources(resources);
+
+      expect(result).toEqual(expected);
+    });
+  });
+
+  describe('setLoading', () => {
+    it('should return a type of setLoading with a boolean', () => {
+      const bool = true;
+      const expected = {
+        type: 'SET_LOADING',
+        isLoading: bool
+      }
+
+      const result = actions.setLoading(bool);
 
       expect(result).toEqual(expected);
     });
