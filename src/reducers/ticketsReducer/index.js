765d285a92da9a13f6e239f59db33eac7d422dc5
RyanDBarnett <conner2468@gmail.com>
2019-07-16 00:24:41 -0600

    Create addTicket case in ticketsReducer and test

--- src/reducers/ticketsReducer/index.js
+++ src/reducers/ticketsReducer/index.js
@@ -2,6 +2,8 @@ export const ticketsReducer = (state = [], action) => {
   switch (action.type) {
     case 'SET_ACTIVE_TICKETS':
       return action.tickets;
+    case 'ADD_TICKET':
+      return [...state, action.ticket]
     default:
       return state;
   }
