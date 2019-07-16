30af654025aa0745bd1996863fd468662777f1d8
RyanDBarnett <conner2468@gmail.com>
2019-07-15 23:49:48 -0600

    Add date input and submit btn to TicketForm

--- src/components/TicketForm/index.js
+++ src/components/TicketForm/index.js
@@ -5,8 +5,14 @@ class TicketForm extends Component {
     super()
     this.state = {
       notes: "",
-      priority: "low"
+      priority: "low",
+      due: ""
     }
+  }
+
+  getTodaysDate = () => {
+    const tzoffset = (new Date()).getTimezoneOffset() * 60000;
+    return (new Date(Date.now() - tzoffset)).toISOString().substr(0, 10);
   }
 
   handleChange = (e) => {
@@ -14,15 +20,15 @@ class TicketForm extends Component {
     this.setState({[name]: value})
   }
 
+  handleSubmit = (e) => {
+    e.preventDefault();
+  }
+
   render() {
     return (
-      <form className="ticket-form">
-        <textarea 
-          name="notes" 
-          onChange={e => this.handleChange(e)} 
-          placeholder="Ticket Description">
-        </textarea>
-        <section onChange={e => this.handleChange(e)} className="priority-btns">
+      <form className="ticket-form" onSubmit={this.handleSubmit} onChange={e => this.handleChange(e)} >
+        <textarea name="notes" placeholder="Ticket Description"></textarea>
+        <section className="priority-btns">
           <input type="radio" id="low" name="priority" value="low" defaultChecked />
           <label htmlFor="low">Low</label>
           <input type="radio" id="medium" name="priority" value="medium" />
@@ -34,6 +40,9 @@ class TicketForm extends Component {
           <input type="radio" name="priority" value="safety" />
           <label htmlFor="safety">Safety</label>
         </section>
+        <label htmlFor="due-date">Due date:</label>
+        <input type="date" id="due-date" name="due" min={this.getTodaysDate()}/>
+        <input type="submit" value="Create New Ticket"/>
       </form>
     )
   }
