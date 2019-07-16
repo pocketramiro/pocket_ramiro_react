b0409a4ffa9d1118708fc70ce56bd68c30455cd9
RyanDBarnett <conner2468@gmail.com>
2019-07-15 20:40:55 -0600

    Connect TicketDashboard and invoke getActiveTickets when the dashboard mounts

--- src/container/TicketDashboard/index.js
+++ src/container/TicketDashboard/index.js
@@ -1,17 +1,32 @@
 import React, { Component } from 'react';
-import Nav from '../../components/Nav/index'
+import Nav from '../../components/Nav/index';
+import { connect } from 'react-redux';
+import { getActiveTickets } from '../../thunks/getActiveTickets'
+
 class TicketDashboard extends Component {
 
+  componentDidMount() {
+    this.props.getActiveTickets();
+  }
 
   render() {
 
     return (
-          <main>
-            <header>
-              <Nav/>
-            </header>
-          </main>
+      <main>
+        <header>
+          <Nav/>
+        </header>
+      </main>
     )
   }
 }
-export default TicketDashboard
+
+const mapStateToProps = (state) => ({
+  tickets: state.tickets
+});
+
+const mapDispatchToProps = (dispatch) => ({
+  getActiveTickets: () => dispatch(getActiveTickets())
+});
+
+export default connect(mapStateToProps, mapDispatchToProps)(TicketDashboard);
