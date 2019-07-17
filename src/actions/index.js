6a71b4d826b644019af6b17d82e4d7a9007009a1
RyanDBarnett <conner2468@gmail.com>
2019-07-16 20:09:47 -0600

    Create setUser action

--- src/actions/index.js
+++ src/actions/index.js
@@ -13,6 +13,11 @@ export const addResource = (resource) => ({
   resource
 });
 
+export const setUser = (user) => ({
+  type: 'SET_USER',
+  user
+});
+
 export const setResources = (resources) => ({
   type: 'SET_RESOURCES',
   resources
