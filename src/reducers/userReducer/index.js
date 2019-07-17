b8617cf294c5cba2808a9db3416f6fb956c4bfdb
RyanDBarnett <conner2468@gmail.com>
2019-07-16 21:30:50 -0600

    Test userReducer

--- src/reducers/userReducer/index.js
+++ src/reducers/userReducer/index.js
@@ -1,7 +1,7 @@
 export const userReducer = (state = {}, action) => {
   switch (action.type) {
     case 'SET_USER':
-      return action.tickets;
+      return action.user;
     case 'CLEAR_USER':
       return {};
     default:
