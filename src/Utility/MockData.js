19115ab8afcfb5cc455329fb927532f61ad69fd9
Michael KS <kingstockton@gmail.com>
2019-07-16 21:42:36 -0600

    Add parts reducer state test

---
+++ src/Utility/MockData.js
@@ -0,0 +1,18 @@
+export const mockPartsResouce = [
+  {
+      "id": 1,
+      "name": "lever 2000",
+      "inventory": 1,
+      "active": true,
+      "created_at": "2019-07-16T13:15:31.885Z",
+      "updated_at": "2019-07-16T13:16:16.146Z"
+  },
+  {
+      "id": 2,
+      "name": "Tank patch",
+      "inventory": 1,
+      "active": true,
+      "created_at": "2019-07-16T13:18:09.048Z",
+      "updated_at": "2019-07-16T13:18:09.048Z"
+  }
+]
--- src/reducers/resourcePartsReducer/resourcePartsReducer.test.js
+++ src/reducers/resourcePartsReducer/resourcePartsReducer.test.js
@@ -1,11 +1,22 @@
 import { resourcePartsReducer } from './';
 import { setResourceParts } from '../../actions';
+import * as MD from '../../Utility/MockData';
 
 describe('resoucePartsReducer', () => {
+
   it('should return a default state', () => {
     const expected = [];
 
     const result = resourcePartsReducer(undefined, [])
     expect(result).toEqual(expected)
   });
+
+  it('should put all resouce parts into state when the case SET_RESOURCE_PARTS', () => {
+    const expected = MD.mockPartsResouce
+    const action = setResourceParts(expected);
+    const result = resourcePartsReducer([], action);
+    
+    expect(result).toEqual(expected);
+  });
+
 });
