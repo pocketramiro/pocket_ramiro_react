a812b0e519e0c0cef8790576282b7485591b45ef
RyanDBarnett <conner2468@gmail.com>
2019-07-17 10:29:37 -0600

    Import getTickets thunk into CardContainer

--- src/components/CardContainer/index.js
+++ src/components/CardContainer/index.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
+import { getTickets } from '../../thunks/getTickets';
 
 export class CardContainer extends Component {
 
