0815c310b194ce5e61ef6516c2d88d192e0b91ca
RyanDBarnett <conner2468@gmail.com>
2019-07-17 12:15:38 -0600

    Display mock cards and begin styling
    Co-Authored-By: Mike <kingstockton@gmail.com>

--- src/components/Card/index.js
+++ src/components/Card/index.js
@@ -1,10 +1,16 @@
 import React from 'react';
 
 export const Card = (props) => {
-  
+  const {notes, priority, created_at} = props.ticket
+  const calcDaysSinceCreation = (t) => Math.floor(t / (24 * 60 * 60 * 1000));
+  const days = calcDaysSinceCreation(Math.abs(new Date() - new Date(created_at)))
+
   return (
     <section>
-      
+      <p className={priority}>Priority: {priority}</p>
+      <p>Opened: {created_at.substr(0, 10)}</p>
+      <p>{`${days} days since ticket opened`}</p>
+      <p>Notes: {notes}</p>
     </section>
   )
 }
--- src/components/CardContainer/index.js
+++ src/components/CardContainer/index.js
@@ -6,17 +6,31 @@ import { Card } from '../Card';
 export class CardContainer extends Component {
 
   componentDidMount() {
-    this.props.getTickets()
+    // this.props.getTickets()
   }
 
   render () {
+    const mockTicket = {
+        "id": 1,
+        "table_key": 15,
+        "table_name": "Resources",
+        "user_id": 1,
+        "notes": "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur",
+        "priority": "low",
+        "frequency_unit": "1",
+        "frequency_value": 0,
+        "created_at": "2019-06-22T22:42:12.000Z",
+        "updated_at": "2019-06-22T22:42:12.000Z",
+        "active": true
+    }
     // Needs to be refactored so different items can be accepted
-    const displayCards = this.props.tickets.map(data => {
-      return <Card key={data.id} data={data}/>
-    })
+    // const displayCards = this.props.tickets.map(data => {
+    //   return <Card key={data.id} data={data}/>
+    // })
     return (
-      <section>
-        {displayCards}
+      <section className='card-container'>
+        <Card ticket={mockTicket}/>
+        <Card ticket={mockTicket}/>
       </section>
     )
   }
