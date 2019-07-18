e20d9111b02f666d750c1a2fd4785d736f12f487
RyanDBarnett <conner2468@gmail.com>
2019-07-17 22:03:40 -0600

    Refactor Nav handleChange and add tickets link

--- src/components/Nav/index.js
+++ src/components/Nav/index.js
@@ -17,34 +17,43 @@ class Nav extends Component {
 
   handleChange = (event) => {
     const { name } = event.target
+    if(!name) return;
     this.setState({title: name , isSelected: false })
   }
 
   render() {
     const {isSelected, title} = this.state
 
-    const mainNavigation = 
-                        <section id='menu'>
-                            <NavLink to='/Assets' name='Assets' onClick={this.handleChange} className='link1'>
-                            <i className="material-icons menu-icons"> insert_chart</i>
-                                Assets
-                            </NavLink>
-                            <NavLink to='/Parts_and_Inventory'name='Parts' onClick={this.handleChange}> 
-                              <i className="material-icons menu-icons"> business_center</i>
-                                Parts
-                            </NavLink>
-                            <NavLink to='/Companies' name='Companies' onClick={this.handleChange}>
-                              <i className="material-icons menu-icons"> business </i>
-                              Companies
-                            </NavLink>
-                          </section>
+    const mainNavigation = (
+      <section id='menu' onClick={this.handleChange}>
+        <NavLink to='/tickets' name='Tickets' className='link1'>
+          <i className="material-icons menu-icons"> insert_chart</i>
+            Tickets
+        </NavLink>
+        <NavLink to='/assets' name='Assets' className='link1'>
+          <i className="material-icons menu-icons"> insert_chart</i>
+            Assets
+        </NavLink>
+        <NavLink to='/parts' name='Parts'> 
+          <i className="material-icons menu-icons"> business_center</i>
+            Parts
+        </NavLink>
+        <NavLink to='/companies' name='Companies'>
+          <i className="material-icons menu-icons"> business </i>
+          Companies
+        </NavLink>
+      </section>
+    );
 
 return (
       <div>
         <section className='nav-wrapper'>
-          <input type='checkbox' className='nav-toggle' id='hamburger'
-                onChange={this.handleSelected}
-                checked={isSelected}
+          <input
+            id='hamburger'
+            type='checkbox' 
+            className='nav-toggle'
+            onChange={this.handleSelected}
+            checked={isSelected}
           />
           <label htmlFor='hamburger'>
             <div className='bar nav-top'></div>
