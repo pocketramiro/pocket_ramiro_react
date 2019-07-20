bafbb45229b014b055355cadb237ef5c546a0904
Michael KS <kingstockton@gmail.com>
2019-07-19 21:44:58 -0600

    Add default state for resourcetypesreducer

--- src/reducers/resourceTypesReducer/resourceTypesReducer.test.js
+++ src/reducers/resourceTypesReducer/resourceTypesReducer.test.js
@@ -1,34 +1,12 @@
-// import { resourcesReducer } from '.';
-// import { addResource, setResourceTypes } from '../../actions';
+import { resourceTypesReducer } from './';
+import { setResourceTypes } from '../../actions';
 
-// describe('resourceReducer', () => {
-//   it('should return default state', () => {
-//     const expected = [];
-//     const result = resourcesReducer(undefined, {});
+describe('resourceTypesReducer', () => {
+  it('should return default state', () => {
+    const expected = [];
+    const result = resourceTypesReducer(undefined, {});
 
-//     expect(result).toEqual(expected);
-//   });
+    expect(result).toEqual(expected);
+  });
 
-//   describe('setResourceTypes case', () => {
-//     it('should set resources in state', () => {
-//       const expected = ['resource 1', 'resource 2'];
-//       const initialState = [];
-//       const action = setResourceTypes(expected);
-//       const result = resourcesReducer(initialState, action);
-
-//       expect(result).toEqual(expected);
-//     });
-//   });
-
-//   describe('addResource case', () => {
-//     it('should add a resource to the existing resources in state', () => {
-//       const newResource = 'resource 3';
-//       const initialState = ['resource 1', 'resource 2'];
-//       const expected = [...initialState, newResource];
-//       const action = addResource(newResource);
-//       const result = resourcesReducer(initialState, action);
-
-//       expect(result).toEqual(expected);
-//     });
-//   });
-// });
+}); 
