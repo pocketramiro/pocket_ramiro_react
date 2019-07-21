00b45040dc669e912853d41c51547b5e1231ad14
Michael KS <kingstockton@gmail.com>
2019-07-21 10:51:32 -0600

    Update style for form

--- src/components/Form/index.js
+++ src/components/Form/index.js
@@ -29,39 +29,48 @@ class Form extends Component {
       notes,
       priority,
     };
+
     //need to make resource second parameter dynamic
     postTicket(newTicket, 1);
   }
 
-  render() {
+  
 
+  render() {
+    const { dataKey } = this.props
     return (
-     
-      <form className="ticket-form" onSubmit={this.handleSubmit} onChange={this.handleChange} >
-        <section className="priority-btns">
-          <input type="radio" id="low" name="priority" value="low" defaultChecked />
-          <label htmlFor="low">Low</label>
-          <input type="radio" id="medium" name="priority" value="medium" />
-          <label htmlFor="medium">Medium</label>
-          <input type="radio" id="high" name="priority" value="high" />
-          <label htmlFor="high">High</label>
-          <input type="radio" id="urgent" name="priority" value="urgent" />
-          <label htmlFor="urgent">Urgent</label>
-          <input type="radio" name="priority" value="safety" />
-          <label htmlFor="safety">Safety</label>
-        </section>
-        <label htmlFor="Notes">Notes
-          <textarea name="notes" placeholder="Ticket Description"></textarea>
-        </label>
-        <input type="submit" value="Create New Ticket"/>
-      </form>
+      <section className='form-container'>
+        <form className="ticket-form" onSubmit={this.handleSubmit} onChange={this.handleChange} >
+          <h1></h1>
+          <section className="priority-btns">
+            <input type="radio" id="low" name="priority" value="low" defaultChecked />
+            <label htmlFor="low">Low</label>
+            <input type="radio" id="medium" name="priority" value="medium" />
+            <label htmlFor="medium">Medium</label>
+            <input type="radio" id="high" name="priority" value="high" />
+            <label htmlFor="high">High</label>
+            <input type="radio" id="urgent" name="priority" value="urgent" />
+            <label htmlFor="urgent">Urgent</label>
+            <input type="radio" name="priority" value="safety" />
+            <label htmlFor="safety">Safety</label>
+          </section>
+          <label htmlFor="Notes">Notes
+            <textarea name="notes" placeholder="Ticket Description"></textarea>
+          </label>
+          <input type="submit" value="Submit Ticket" className='submit-btn'/>
+        </form>
+      </section>
     );
   }
 }
 
-export const mapStateToProps = (store) => ({
-  user: store.user
-});
+export const mapStateToProps = (state, otherProps) => {
+ const { dataKey } = otherProps;
+
+  return {
+    [dataKey]: state[dataKey]
+  };
+};
 
 export const mapDispatchToProps = (dispatch) => ({
   postTicket: (ticket, id) => dispatch(postTicket(ticket, id))
