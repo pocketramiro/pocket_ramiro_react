efe2a35ef219600444ce9b9982e9b5014e144fcb
RyanDBarnett <conner2468@gmail.com>
2019-07-22 19:13:16 -0600

    Persist redux state to localstorage

--- src/index.js
+++ src/index.js
@@ -10,7 +10,13 @@ import { composeWithDevTools } from 'redux-devtools-extension';
 import thunk from 'redux-thunk';
 import {rootReducer} from './reducers/index';
 
-const store = createStore(rootReducer, composeWithDevTools(applyMiddleware(thunk)));
+const persistedState = localStorage.getItem('reduxState') ? JSON.parse(localStorage.getItem('reduxState')) : {}
+
+const store = createStore(rootReducer, persistedState, composeWithDevTools(applyMiddleware(thunk)))
+
+store.subscribe(() => {
+  localStorage.setItem('reduxState', JSON.stringify(store.getState()))
+})
 
 const router = (
   <Provider store={store}>
