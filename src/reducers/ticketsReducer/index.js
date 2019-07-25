ab7c16ee964620d282d0e241527bc9a0a9d45b5d
RyanDBarnett <conner2468@gmail.com>
2019-07-24 18:44:42 -0600

    Fix add ticket reducer test

--- src/reducers/ticketsReducer/index.js
+++ src/reducers/ticketsReducer/index.js
@@ -1,11 +1,11 @@
-export const ticketsReducer = (state = [], action) => {
+export const ticketsReducer = (state = {}, action) => {
   switch (action.type) {
   case 'SET_TICKETS':
     return action.tickets;
   case 'ADD_TICKET':
     const newState = state;
     const newTicket = {id: action.ticket.id, type: 'ticket' , attributes: action.ticket};
-    newState.data = [...state.data, newTicket];
+    newState.data = state.data && [...state.data, newTicket];
     return newState;
   case 'CLEAR_TICKETS':
     return [];
--- src/reducers/ticketsReducer/ticketsReducer.test.js
+++ src/reducers/ticketsReducer/ticketsReducer.test.js
@@ -3,27 +3,26 @@ import { setTickets, addTicket } from '../../actions';
 
 describe('ticketsReducer', () => {
   it('should return default state', () => {
-    const expected = [];
-    const result = ticketsReducer(undefined, []);
+    const expected = {};
+    const result = ticketsReducer(undefined, {});
 
     expect(result).toEqual(expected);
   });
 
   it('should is load all the tickets into state when the case is SET_TICKETS', () => {
-    const expected = ['ticket1', 'ticket2'];
+    const expected = {data: ['ticket1', 'ticket2'], included: []};
     const action = setTickets(expected);
-    const result = ticketsReducer([], action);
+    const result = ticketsReducer({}, action);
 
     expect(result).toEqual(expected);
   });
 
   it('should add a ticket on the state tree when the case is ADD_TICKET', () => {
-    const newTicket = {'name': 'test ticket'};
-    const currentState = ['ticket1', 'ticket2'];
-    const expected = [...currentState, newTicket];
+    const newTicket = {id: 3, 'name': 'ticket3'};
+    const currentState = {data: []};
+    const expected = {attributes: newTicket, type: 'ticket', id: newTicket.id};
     const action = addTicket(newTicket);
     const result = ticketsReducer(currentState, action);
-
-    expect(result).toEqual(expected);
+    expect(result.data[0]).toEqual(expected);
   });
 });
